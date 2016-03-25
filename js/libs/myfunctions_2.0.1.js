664823a173cc1e9a372c86b126adc5b0a12b29ce
derOptimist <github@schwitte.de>
2016-03-25 15:38:23 +0100

    s

--- js/libs/myfunctions_2.0.1.js
+++ js/libs/myfunctions_2.0.1.js
@@ -18,6 +18,26 @@ $.mobile.defaultDialogTransition = 'none';
 $.mobile.useFastClick = true; 
 $.mobile.touchOverflowEnabled = true;
 alert("ready");
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+
+checkConnection();
+
+
 if (window.localStorage.getItem("chat_aktiv") == 0)
 {
    var neuerLink = document.createElement("label");
